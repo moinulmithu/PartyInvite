2b0e6353ac5940594da93502abca5060ff12fd28
moinulmithu <moinulmithu@hotmail.com>
2015-01-05 10:58:20 +0600

    PartyInvite

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage= "You must enter your Name !!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Your Email address can't be empty")]
+        [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter your Phone No.")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Please specify wheather you come or not????")]
+        public bool? WillAttend { get; set; }
+    }
+}
