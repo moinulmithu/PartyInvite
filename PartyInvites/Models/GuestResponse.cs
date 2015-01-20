0f12ede948a60b7e5882ddeec77906b551554216
moinulmithu <moinulmithu@hotmail.com>
2015-01-20 09:18:08 +0600

    gftf

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -8,9 +8,9 @@ namespace PartyInvites.Models
 {
     public class GuestResponse
     {
-        [Required(ErrorMessage= "You must enter your Name !!")]
+        [Required(ErrorMessage= "You must enter your Name first")]
         public string Name { get; set; }
-        [Required(ErrorMessage ="Your Email address can't be empty")]
+        [Required(ErrorMessage ="Your Email address can't be empty. please enter a valid email address")]
         [RegularExpression(".+\\@.+\\..+",ErrorMessage = "Please enter a valid email address")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Please enter your Phone No.")]
